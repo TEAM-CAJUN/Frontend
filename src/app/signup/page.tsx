bca217e410f02f76c9a10a626a676a8d4a1085ae
UBiiiii <willowman55@gmail.com>
2023-09-12 16:18:55 +0900

    [feat] #2 sign up, sign in page 생성 및 기본 css, 동작 구성

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import Button from "@/components/button/Button";
+import { FormEventHandler } from "react";
+
+interface inputProps {
+  label: string,
+  name: string,
+  type?: string | "text",
+}
+
+const InputBlock = ({ label, name, type }: inputProps) => {
+  return (
+    <div className="flex justify-start items-center self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 px-[22px]">
+      <div className="flex flex-shrink-0 justify-center items-center w-24 h-full">
+        {label}
+      </div>
+      <div className="flex justify-start items-start flex-grow h-[50px] relative overflow-hidden gap-2.5 px-2.5 py-[5px]">
+        <input name={name} type={type} className="self-stretch flex-grow w-full h-10 relative overflow-hidden rounded-[14px] border-2 border-[#2e9bff] px-2" />
+      </div>
+    </div>
+  )
+}
+
+const Signup = () => {
+
+  const infoList : inputProps[] = [{
+    label:"아이디(이메일)",
+    name:"bdsemail",
+  }, {
+    label:"비밀번호",
+    name:"bdspassword",
+    type:"password"
+  },{
+    label:"비밀번호 확인",
+    name:"bdscheck",
+    type:"password"
+  },{
+    label:"이름",
+    name:"bdsname",
+  },{
+    label:"생년월일",
+    name:"bdsbirth",
+    type:"date",
+  },{
+    label:"연락처",
+    name:"bdsphone",
+  },];
+
+  const handleSubmit : FormEventHandler = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target as HTMLFormElement);
+    console.log("Sign Up");
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center flex-grow w-[1024px] overflow-hidden px-5">
+      <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden px-10 py-5 rounded-[18px] border-4 border-[#2e81ff]">
+        <div className="self-stretch flex-grow-0 flex-shrink-0 h-[50px] relative overflow-hidden text-2xl">
+          회원가입
+        </div>
+        <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden px-10 py-5 border-t-0 border-r-0 border-b border-l-0 border-[#2e9bff]">
+          {infoList.map((el,i)=>(
+            <InputBlock key={i} {...el}/>
+          ))}
+        </div>
+        <div className="w-full h-fit py-2 flex flex-shrink-0 justify-center items-center">
+          <Button text={"회원가입"} />
+        </div>
+      </form>
+    </div>
+  )
+};
+
+export default Signup;
--- src/components/list/ListItem.tsx
+++ src/components/list/ListItem.tsx
@@ -1,63 +1,63 @@
 export interface ItemProps {
-    name: string,
-    address: string,
-    isDeal: number,
-    isInsurance: number,
-    isLoan: number,
-    isPaper: number,
-    isBpaper: number,
-    isPhoto?: number,
+  name: string,
+  address: string,
+  isDeal: number,
+  isInsurance: number,
+  isLoan: number,
+  isPaper: number,
+  isBpaper: number,
+  isPhoto?: number,
 }
 
 interface bannerProps {
-    name: string,
-    status: number,
+  name: string,
+  status: number,
 }
 
 const theme = [
-    {
-        bg: "bg-blue-100",
-        text: "text-blue-800",
-    },
-    {
-        bg: "bg-red-100",
-        text: "text-red-800",
-    },
-    {
-        bg: "bg-green-100",
-        text: "text-green-800",
-    },
+  {
+    bg: "bg-blue-100",
+    text: "text-blue-800",
+  },
+  {
+    bg: "bg-red-100",
+    text: "text-red-800",
+  },
+  {
+    bg: "bg-green-100",
+    text: "text-green-800",
+  },
 ];
 
-const StatusBanner = ({name, status} : {name:string, status: number}) => {
-    return(
-        <div className={`flex justify-center items-center flex-grow-0 flex-shrink-0 relative px-3 py-0.5 rounded-xl ${theme[status]?.bg || "bg-black-100"}`}>
-            <p className={`flex-grow-0 flex-shrink-0 text-sm font-medium text-center ${theme[status]?.text || "text-black-800"}`}>
-                {name}
-            </p>
-        </div>
-    )
+const StatusBanner = ({ name, status }: { name: string, status: number }) => {
+  return (
+    <div className={`flex justify-center items-center flex-grow-0 flex-shrink-0 relative px-3 py-0.5 rounded-xl ${theme[status]?.bg || "bg-black-100"}`}>
+      <p className={`flex-grow-0 flex-shrink-0 text-sm font-medium text-center ${theme[status]?.text || "text-black-800"}`}>
+        {name}
+      </p>
+    </div>
+  )
 }
 
 const ListItem = (props: ItemProps) => {
-    const bannerList = [{name:'등기부등본', status:props.isPaper}, {name:'건축물대장', status:props.isBpaper}, {name:'전세대출 가능', status:props.isLoan}, {name:'보증보험 가입', status:props.isInsurance},]
-    return (
-        <div
-            className=" box-content flex justify-start items-center self-stretch flex-grow-0 flex-shrink-0 h-[150px] relative overflow-hidden gap-2.5 p-[15px] rounded-[15px] border-2 border-blue-400 hover:border-blue-500 shadow-md hover:shadow-lg"
-        >
-            <div className="flex-grow-0 flex-shrink-0 w-[120px] h-[120px] relative overflow-hidden bg-[#d9d9d9]">
-                <p className="absolute left-12 top-[52px] text-xs font-medium text-left text-black">사진</p>
-            </div>
-            <div className="flex flex-col justify-center items-start self-stretch flex-grow relative overflow-hidden gap-2.5 py-[5px]">
-                <div className="self-stretch flex-grow relative overflow-hidden">
-                        주소 : 서울특별시 동대문구 망우로18가길 82-39
-                </div>
-                <div className="flex justify-start items-center h-fit flex-grow overflow-hidden gap-2.5 p-2.5">
-                    {bannerList.map((el,i)=><StatusBanner key={i} {...el}/>)}
-                </div>
-            </div>
+  const bannerList = [{ name: '등기부등본', status: props.isPaper }, { name: '건축물대장', status: props.isBpaper }, { name: '전세대출 가능', status: props.isLoan }, { name: '보증보험 가입', status: props.isInsurance },]
+  return (
+    <div
+      className=" box-content flex justify-start items-center self-stretch flex-grow-0 flex-shrink-0 h-[150px] relative overflow-hidden gap-2.5 p-[15px] rounded-[15px] border-2 border-blue-400 hover:border-blue-500 shadow-md hover:shadow-lg"
+    >
+      <div className="flex-grow-0 flex-shrink-0 w-[120px] h-[120px] relative overflow-hidden bg-[#d9d9d9]">
+        <p className="absolute left-12 top-[52px] text-xs font-medium text-left text-black">사진</p>
+      </div>
+      <div className="flex flex-col justify-center items-start self-stretch flex-grow relative overflow-hidden gap-2.5 py-[5px]">
+        <div className="self-stretch flex-grow relative overflow-hidden">
+          주소 : 서울특별시 동대문구 망우로18가길 82-39
         </div>
-    )
+        <div className="flex justify-start items-center h-fit flex-grow overflow-hidden gap-2.5 p-2.5">
+          {bannerList.map((el, i) => <StatusBanner key={i} {...el} />)}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ListItem;
